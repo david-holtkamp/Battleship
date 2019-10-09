a10a56e2baaeca3f5968283f91dec29ab5b0530d
DeMarcus Kirby <tkd024@gmail.com>
2019-10-09 13:07:46 -0600

    Refactor Ship Test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -4,52 +4,48 @@ require 'minitest/pride'
 require_relative '../ship.rb'
 
 class ShipTest < Minitest::Test
+  def setup
+    @ship = Ship.new('Cruiser', 3)
+  end
   def test_it_exist
-    ship = Ship.new('Cruiser', 3)
-    assert_instance_of Ship, ship
+    assert_instance_of Ship, @ship
   end
 
   def test_it_has_a_name
-    ship = Ship.new('Cruiser', 3)
-    assert_equal "Cruiser", ship.name
+    assert_equal "Cruiser", @ship.name
   end
 
   def test_it_has_a_correct_length
-    ship = Ship.new('Cruiser', 3)
-    assert_equal 3, ship.length 
+    assert_equal 3, @ship.length 
   end
 
   def test_health_equal_to_length
-    ship = Ship.new('Cruiser', 3)
     ship2 = Ship.new('Tugboat', 2)
-    assert_equal 3, ship.health
+    assert_equal 3, @ship.health
     assert_equal 2, ship2.health
   end
 
   def test_it_starts_as_not_sunk
-    ship = Ship.new('Cruiser', 3)
-    assert_equal false, ship.sunk?
+    assert_equal false, @ship.sunk?
   end
 
   def test_health_is_lost_when_hit
-    ship = Ship.new('Cruiser', 3)
-    assert_equal 3, ship.health
-    ship.hit
-    assert_equal 2, ship.health
+    assert_equal 3, @ship.health
+    @ship.hit
+    assert_equal 2, @ship.health
   end
 
   def test_ship_is_sunk_when_health_is_zero
-    ship = Ship.new('Cruiser', 3)
-    assert_equal 3, ship.health
-    assert_equal false, ship.sunk?
-    ship.hit
-    assert_equal 2, ship.health
-    assert_equal false, ship.sunk?
-    ship.hit
-    assert_equal 1, ship.health
-    assert_equal false, ship.sunk?
-    ship.hit
-    assert_equal 0, ship.health
-    assert_equal true, ship.sunk?
+    assert_equal 3, @ship.health
+    assert_equal false, @ship.sunk?
+    @ship.hit
+    assert_equal 2, @ship.health
+    assert_equal false, @ship.sunk?
+    @ship.hit
+    assert_equal 1, @ship.health
+    assert_equal false, @ship.sunk?
+    @ship.hit
+    assert_equal 0, @ship.health
+    assert_equal true, @ship.sunk?
   end
 end
